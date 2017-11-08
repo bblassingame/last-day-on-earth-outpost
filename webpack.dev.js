201e42a16d6349f2b8937a867f0d4e642f65d1f3
Bo Blassingame <bo.blassingame@gmail.com>
2017-11-07 21:34:38 -0600

    Updated the repo name to use the new Outpost name and added a proxy server to try to set up the api with my local apache server

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,7 +3,15 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   devtool: 'inline-source-map',
+
+
   devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://other-server.example.com',
+        secure: false
+      }
+    },
     contentBase: './bin',
     historyApiFallback: true
   },
