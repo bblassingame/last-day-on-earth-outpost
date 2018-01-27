ef15bdcc83df84c46d6671f6568cc1e66faf659b
Bo Blassingame <bo.blassingame@gmail.com>
2018-01-26 22:42:25 -0600

    Updated babel and webpack to work with updated react-hot-loader package

--- webpack.common.js
+++ webpack.common.js
@@ -1,56 +1,53 @@
-var webpack = require('webpack');
-var path = require('path');
+var webpack = require('webpack')
+var path = require('path')
 
-const BUILD_DIR = path.resolve(__dirname, 'bin/');
-const APP_DIR = path.resolve(__dirname, 'src/application/');
-const ENTRY_PATH = path.resolve(APP_DIR, 'application.js');
+const BUILD_DIR = path.resolve(__dirname, 'bin/')
+const APP_DIR = path.resolve(__dirname, 'src/application/')
+const ENTRY_PATH = path.resolve(APP_DIR, 'application.js')
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: ['babel-polyfill', ENTRY_PATH],
-    output: {
-        path: BUILD_DIR,
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    module: {
-        rules:
+  entry: ['babel-polyfill', 'react-hot-loader/patch', ENTRY_PATH],
+  output: {
+    path: BUILD_DIR,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules:
+    [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use:
         [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use:
-                [
-                    {
-                        loader: 'react-hot-loader'
-                    },
-                    {
-                        loader: 'babel-loader',
-                        options: {
-                            presets: ['es2015', 'react']
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                loader: 'style-loader!css-loader'
-            },
-            {
-                test: /\.(gif|png|jpg)$/,
-                exclude: /node_modules/,
-                loader: 'file-loader'
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015', 'react']
             }
+          }
         ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(['bin']),
-        new HtmlWebpackPlugin({
-            title: 'LDOE: Outpost',
-            template: './src/template.ejs'
-        })
-    ],
-};
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.(gif|png|jpg)$/,
+        exclude: /node_modules/,
+        loader: 'file-loader'
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['bin']),
+    new HtmlWebpackPlugin({
+      title: 'LDOE: Outpost',
+      template: './src/template.ejs'
+    })
+  ],
+}
