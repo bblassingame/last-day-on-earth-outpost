e68f3cce4ccda84ba6023044d78a78e7f5162527
Bo Blassingame <bo.blassingame@gmail.com>
2018-04-26 18:50:27 -0500

    Added webpack config to support new files added for favicon support

--- webpack.prod.js
+++ webpack.prod.js
@@ -15,7 +15,16 @@ module.exports = merge(common, {
     new CopyWebpackPlugin([
       {from: 'sitemap.xml', to: 'sitemap.xml'},
       {from: 'BingSiteAuth.xml', to: 'BingSiteAuth.xml'},
-      {from: 'google51ddbd8819b54a7d.html', to: 'google51ddbd8819b54a7d.html'}
+      {from: 'google51ddbd8819b54a7d.html', to: 'google51ddbd8819b54a7d.html'},
+      {from: 'browserconfig.xml', to: 'browserconfig.xml'},
+      {from: 'favicon.ico', to: 'favicon.ico'},
+      {from: 'safari-pinned-tab.svg', to: 'safari-pinned-tab.svg'},
+      {from: 'site.webmanifest', to: 'site.webmanifest'},
+      {from: 'android-chrome-144x144.png', to: 'android-chrome-144x144.png'},
+      {from: 'apple-touch-icon.png', to: 'apple-touch-icon.png'},
+      {from: 'favicon-16x16.png', to: 'favicon-16x16.png'},
+      {from: 'favicon-32x32.png', to: 'favicon-32x32.png'},
+      {from: 'mstile-150x150.png', to: 'mstile-150x150.png'},
     ]),
   ],
 })
