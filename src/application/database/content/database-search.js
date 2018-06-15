49b85088b4cbcff282382e5183d9e9fc79a8ac78
Bo Blassingame <bo.blassingame@gmail.com>
2018-06-15 11:16:08 -0500

    Add autofocus on the search bar in the database application

--- src/application/database/content/database-search.js
+++ src/application/database/content/database-search.js
@@ -6,7 +6,7 @@ const DatabaseSearch = (props) => {
   return (
     <div className='database-search-container'>
       <img className='database-search-icon' src={SearchIcon} alt='A magnifying glass signifying a search input' />
-      <input onInput={(event) => props.onInput(event)} className='database-search-input' type='text' name='search' placeholder='Search...' />
+      <input onInput={(event) => props.onInput(event)} className='database-search-input' type='text' name='search' placeholder='Search...' autoFocus />
     </div>
   )
 }
